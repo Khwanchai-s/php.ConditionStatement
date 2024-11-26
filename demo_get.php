fb406a706efd9275c1b3fdeda68fa3e80d6ad6f3
Khwanchai-s <khwanchai.s@gmail.com>
2024-11-26 11:26:07 +0700

    update 26/11/2024

---
+++ demo_get.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>สร้างฟอร์มสำหรับทดสอบ Method GET</title>
+</head>
+<body>
+    <h2>โปรดกรอกข้อมูลลงในฟอร์ม</h2>
+    <form action="test_get.php" method="get">
+        ชื่อวิชา : <input type="text" name="subject"> <br>
+        เว็บไซต์ที่แนะนำ : <input type="text" name="web"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
